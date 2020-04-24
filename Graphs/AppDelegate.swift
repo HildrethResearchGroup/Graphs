2ce364ceddda2e1147f2d775f9c075e8926a53c3
seeaya <connorbarnesdenver@icloud.com>
2020-04-23 21:07:08 -0600

    Initial Commit

---
+++ Graphs/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Graphs
+//
+//  Created by Connor Barnes on 4/23/20.
+//  Copyright © 2020 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Graphs/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Graphs
+//
+//  Created by Connor Barnes on 4/23/20.
+//  Copyright © 2020 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
