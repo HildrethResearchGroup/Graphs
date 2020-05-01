b39a3c5016839df0738e5d69cf79eb0c6e950b02
seeaya <connorbarnesdenver@icloud.com>
2020-04-30 22:11:39 -0600

    Added pasteboard writer for Directory drag & drop.

---
+++ Graphs/View Controllers/Source List/DirectoryPasteboardItem.swift
@@ -0,0 +1,36 @@
+//
+//  DirectoryPasteboardItem.swift
+//  Graphs
+//
+//  Created by Connor Barnes on 4/30/20.
+//  Copyright © 2020 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+class DirectoryPasteboardWriter: NSFilePromiseProvider {
+	enum UserInfoKeys {
+		
+	}
+	
+	// MARK: NSPasteboardWriting
+	
+	override func writableTypes(for pasteboard: NSPasteboard) -> [NSPasteboard.PasteboardType] {
+		var types = super.writableTypes(for: pasteboard)
+		types.append(.directoryRowPasteboardType)
+		
+		return types
+	}
+	
+	override func pasteboardPropertyList(forType type: NSPasteboard.PasteboardType) -> Any? {
+		switch type {
+		case .directoryRowPasteboardType:
+			// The pasteboard type is the internal directory drag, use userInfo as the property list
+			return userInfo
+		default:
+			// The pasteboard type could be a file promise, the super class can determine the property list.
+			return super.pasteboardPropertyList(forType: type)
+		}
+	}
+	
+}
