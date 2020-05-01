7e61fb796a8accb4460db0110cedb8103e5ae2e2
seeaya <connorbarnesdenver@icloud.com>
2020-04-30 21:52:46 -0600

    Added stubs for drag and drop support for the source-list's NSOutlineView for reordering.

---
+++ Graphs/Controllers/SidebarViewController+DragDrop.swift
@@ -0,0 +1,31 @@
+//
+//  SidebarViewController+DragDrop.swift
+//  Graphs
+//
+//  Created by Connor Barnes on 4/30/20.
+//  Copyright © 2020 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+extension SidebarViewController {
+	func outlineView(_ outlineView: NSOutlineView, pasteboardWriterForItem item: Any) -> NSPasteboardWriting? {
+		#warning("Not implemented")
+		fatalError("Not implemented")
+	}
+	
+	func outlineView(_ outlineView: NSOutlineView, validateDrop info: NSDraggingInfo, proposedItem item: Any?, proposedChildIndex index: Int) -> NSDragOperation {
+		#warning("Not implemented")
+		fatalError("Not implemented")
+	}
+	
+	func outlineView(_ outlineView: NSOutlineView, acceptDrop info: NSDraggingInfo, item: Any?, childIndex index: Int) -> Bool {
+		#warning("Not implemented")
+		fatalError("Not implemented")
+	}
+	
+	func outlineView(_ outlineView: NSOutlineView, draggingSession session: NSDraggingSession, endedAt screenPoint: NSPoint, operation: NSDragOperation) {
+		#warning("Not implemented")
+		fatalError("Not implemented")
+	}
+}
