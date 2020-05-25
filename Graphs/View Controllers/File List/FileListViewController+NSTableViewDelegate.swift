6b019037501df5409eb197b570f1297736e5c95f
seeaya <connorbarnesdenver@icloud.com>
2020-05-24 22:00:09 -0600

    Implemented basic user interface for the file list view controller.

---
+++ Graphs/View Controllers/File List/FileListViewController+NSTableViewDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  FileListViewController+NSTableViewDelegate.swift
+//  Graphs
+//
+//  Created by Connor Barnes on 5/24/20.
+//  Copyright © 2020 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+extension FileListViewController: NSTableViewDataSource {
+	
+}
+
+extension FileListViewController: NSTableViewDelegate {
+	
+}
---
+++ Graphs/View Controllers/File List/FileListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FileListViewController.swift
+//  Graphs
+//
+//  Created by Connor Barnes on 5/24/20.
+//  Copyright © 2020 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+class FileListViewController: NSViewController {
+	
+}
