75dab8e861f624c09fedaa4f1f815fd12bb55f7b
seeaya <connorbarnesdenver@icloud.com>
2020-06-16 23:08:04 -0600

    Added classes and outlets for parser inspector outline view cells.

--- Graphs/Views/Inspector Cells.swift
+++ Graphs/Views/Inspector Cells.swift
@@ -25,3 +25,39 @@ class InspectorTwoPopUpButtonsCell: NSTableCellView {
 class InspectorTextViewCell: NSTableCellView {
 	@IBOutlet weak var textView: NSTextView!
 }
+
+class InspectorTableCell: NSTableCellView {
+	@IBOutlet weak var tableView: NSTableView!
+	@IBOutlet weak var addItemButton: NSButton!
+	@IBOutlet weak var removeItemButton: NSButton!
+}
+
+class InspectorCategoryButtonCell: NSTableCellView {
+	@IBOutlet weak var button: NSButton!
+}
+
+class InspectorTwoNumberFieldsOneOptionCell: NSTableCellView {
+	@IBOutlet weak var firstTextField: NSTextField!
+	@IBOutlet weak var secondTextField: NSTextField!
+	@IBOutlet weak var firstStepper: NSStepper!
+	@IBOutlet weak var secondStepper: NSStepper!
+	@IBOutlet weak var popUpOption: NSPopUpButton!
+}
+
+class InspectorTwoNumberFieldsOneOptionOnePopUpCell: NSTableCellView {
+	@IBOutlet weak var firstTextField: NSTextField!
+	@IBOutlet weak var secondTextField: NSTextField!
+	@IBOutlet weak var firstStepper: NSStepper!
+	@IBOutlet weak var secondStepper: NSStepper!
+	@IBOutlet weak var popUpOption: NSPopUpButton!
+	@IBOutlet weak var popUpButton: NSPopUpButton!
+}
+
+class InspectorOneNumberFieldOnePopUpCell: NSTableCellView {
+	@IBOutlet weak var stepper: NSStepper!
+	@IBOutlet weak var popUpButton: NSPopUpButton!
+}
+
+class InspectorOneNumberFieldCell: NSTableCellView {
+	@IBOutlet weak var stepper: NSStepper!
+}
