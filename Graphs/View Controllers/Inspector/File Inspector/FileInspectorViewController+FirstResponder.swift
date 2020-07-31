cc4491113cd4fe3058f30d2adb7f4076e604b7e9
seeaya <connorbarnesdenver@icloud.com>
2020-07-31 02:20:00 -0600

    Added "show in Finder" to the File Inspector

---
+++ Graphs/View Controllers/Inspector/File Inspector/FileInspectorViewController+FirstResponder.swift
@@ -0,0 +1,32 @@
+//
+//  FileInspectorViewController+FirstResponder.swift
+//  Graphs
+//
+//  Created by Connor Barnes on 7/31/20.
+//  Copyright © 2020 Connor Barnes. All rights reserved.
+//
+
+import Cocoa
+
+extension FileInspectorViewController {
+	/// Shows the selected file in Finder if it exists.
+	@objc func showInFinder(_ sender: Any?) {
+		guard let path = file?.path else { return }
+		NSWorkspace.shared.activateFileViewerSelecting([path])
+	}
+}
+
+extension FileInspectorViewController: NSUserInterfaceValidations {
+	func validateUserInterfaceItem(_ item: NSValidatedUserInterfaceItem) -> Bool {
+		switch item.action {
+		case #selector(showInFinder(_:)):
+			// If there inspector isn't showing a file (file == nil), or if the file doesn't have a path, then it can't be show in Finder
+			guard let path = file?.path else { return false }
+			// Only allow showing in Finder if a file exists at the file's given path
+			return FileManager.default.fileExists(atPath: path.path)
+		default:
+			// Many actions can always be performed. Instead of switching over each one and returning true, we can by default return true and add swtich cases for when false should (sometimes) be returned
+			return true
+		}
+	}
+}
